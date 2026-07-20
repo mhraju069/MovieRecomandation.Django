762272e2622b34872f619e9485953ebf8cd824e2
Raju <mhraju069@gmail.com>
2026-07-20 12:47:04 +0600

    Update: models.py - Update otp digit 6 to 4

--- authentication/models.py
+++ authentication/models.py
@@ -105,7 +105,7 @@ class OTP(models.Model):
 
     @staticmethod
     def generate_otp(user):
-        otp_code = str(random.randint(100000, 999999))
+        otp_code = str(random.randint(1000, 9999))
         return OTP.objects.create(user=user, otp=otp_code)
 
     def is_expired(self):
